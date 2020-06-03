712b84603f19879610c1448a31bf5caef99f688c
stephanie <stephieplusthree@gmail.com>
2020-06-02 19:15:44 -0700

    ready for heroku

--- src/js/rootStore.js
+++ src/js/rootStore.js
@@ -7,3 +7,6 @@ const rootStore = createStore(
 );
 
 export default rootStore;
+
+//create the store and attach any middleware-redux
+//Redux DevTools will be able to see all the changes that pass through the Store
